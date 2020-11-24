7ddbb1639600b730619fa20585f6f89bf8f269e4
agathasta <kalidonia@hotmail.com>
2020-11-24 18:16:11 +0100

    First set-up

---
+++ mastermind.rb
@@ -0,0 +1,19 @@
+# Set up the board with 4 holes and 12 lines BOARD
+# Set up the counter with 4 holes and 12 lines (maybe ? for white, ! for red and - for empty) BOARD
+# Let computer make random number combination (1..6) PLAYER
+# Loop
+#   Get number combination from player (1..6) PLAYER
+#   Check if any positions are right  BOARD
+#   Check if any positions are right  BOARD
+#   Show answer GAME
+#   Break if game is over GAME
+# Show solution GAME
+
+class Mastermind
+end
+
+class Player
+end
+
+class Board
+end
